ef560a942c4154052e2ade1cfb19e79562d617d2
Daniele Treccani <51238740+DanTRK@users.noreply.github.com>
2025-01-10 15:27:20 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,47 @@
 var addressPoints = [
   [
-    "St. Mark's Basilica - survey - Venice",
+    "St. Mark's Basilica (Venice) - survey 2018/2025",
     45.434594535890966,
     12.339742056404038
   ],
   [
-    "Ducale Palace - survey - Venice",
+    "Ducale Palace (Venice) - survey 2025",
     45.43372299423483,
     12.340385336681752
   ],
   [
-    "Terracorpo Castle - survey - Caserta",
+    "Terracorpo Castle (Caserta) - survey 2023",
     41.31368532635118,
     14.030226252700746
   ],
   [
-    "St. Andrea Basilica - survey - Mantova",
+    "St. Andrea Basilica (Mantova) - monitoring 2018/2025",
     45.15890448277335,
     10.794386115742203
   ],
   [
-    "St. Peter Cathedral - survey - Mantova",
+    "St. Peter Cathedral (Mantova) - survey+HBIM 2018",
     45.16061990202021,
     10.79731519431784
+  ],
+  [
+    "Arc of Augusto (Aosta) - survey+HBIM 2019",
+    45.739321019117064,
+    7.328146064419771
+  ],
+  [
+    "Sabbioneta - survey 2020",
+    44.99828742870856,
+    10.490998510533982
+  ],
+  [
+    "3D Geoinfo 2024 (Vigo) - conference",
+    42.22740397951454,
+    -8.734829634395538
+  ],
+  [
+    "LowCost 3D workshop 2024 (Brescia) - conference",
+    45.54408159222205,
+    10.218860585195127
   ]
 ];
