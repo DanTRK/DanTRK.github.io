678335aa4b411b2d672838b011fa21cc599bb9d2
Daniele Treccani <51238740+DanTRK@users.noreply.github.com>
2026-07-03 13:43:59 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,152 +1,73 @@
 var addressPoints = [
-  [
-    "St. Mark's Basilica (Venice) - survey 2018/2025",
-    45.434594535890966,
-    12.339742056404038
-  ],
-  [
-    "Ducale Palace (Venice) - survey 2025",
-    45.43372299423483,
-    12.340385336681752
-  ],
-  [
-    "Terracorpo Castle (Caserta) - survey 2023",
-    41.31368532635118,
-    14.030226252700746
-  ],
-  [
-    "St. Andrea Basilica (Mantova) - monitoring 2018/2025",
-    45.15890448277335,
-    10.794386115742203
-  ],
-  [
-    "St. Peter Cathedral (Mantova) - survey+HBIM 2018",
-    45.16061990202021,
-    10.79731519431784
-  ],
-  [
-    "Arc of Augusto (Aosta) - survey+HBIM 2019",
-    45.739321019117064,
-    7.328146064419771
-  ],
-  [
-    "3D Geoinfo 2024 (Vigo) - conference",
-    42.22740397951454,
-    -8.734829634395538
-  ],
-  [
-    "LowCost 3D workshop 2024 (Brescia) - conference",
-    45.54408159222205,
-    10.218860585195127
-  ],
-  [
-    "Cortile della Cavallerizza (Mantova) - survey 2018",
-    45.15992807780407,
-    10.799964196254171
-  ],
-  [
-    "Camera degli sposi (Mantova) - 2020 survey",
-    45.161124658603605,
-    10.800016182953703
-  ],
-  [
-    "Holy Sepulchre Church (Jerusalem) - survey+HBIM 2021",
-    31.77850737603422,
-    35.229602399546174
-  ],
-  [
-    "Tempio di San Sebastiano (Mantova) - survey 2019",
-    45.15105091074389,
-    10.788113953848386
-  ],
-  [
-    "GeoAI 2024 (Trento) - Conference",
-    46.06694048848798,
-    11.15135493340209
-  ],
-  [
-    "GeoAI 2023 (Torino) - onference",
-    45.06756453176935,
-    7.6563545658953736
-  ],
-  [
-    "SCANNER Workshop 2024 (Pogoni) - Summer school",
-    40.03377260361343,
-    20.43838405057237
-  ],
-  [
-    "H-BI(M)ON 2025 (Parma) - Conference",
-    44.766296679678184,
-    10.309571287812796
-  ],
-  [
-    "ISPRS Congress 2022 (Nice) - Conference",
-    43.70412963680531,
-    7.280434813422904
-  ],
-  [
-    "CIPA Symposium 2023 (Florence) - Conference",
-    43.80098301727668,
-    11.245345697490501
-  ],
-  [
-    "Point Cloud Processing PCP workshop 2019 (Stuttgart) - Conference",
-    48.74573947433458,
-    9.108518254847908
-  ],
-  [
-    "AI4CH Artificial Intelligence in Geomatics for Cultural Heritage 2019 (Turin) - Conference",
-    45.05353705867574,
-    7.686619104065213
-  ],
-  [
-    "Palazzo dell'Arengario (Monza) - survey 2021",
-    45.584510534261476,
-    9.275199938165667
-  ],
-  [
-    "3D-Arch 2022 (Mantova) - Conference",
-    45.156793221796455,
-    10.781965400912256
-  ],
-  [
-    "Roman villa (Negrar di Valpolicella) - survey 2022",
-    45.53834196254279,
-    10.945243452280152
-  ],
-  [
-    "San Giovanni Evangelista Church (Parma) - survey 2022",
-    44.80285747339115,
-    10.332623336224444
-  ],
-  [
-    "San Michele Maggiore Church (Pavia) - survey 2023",
-    45.18221154374779,
-    9.156673292021898
-  ],
-  [
-    "MMT 2023 - 2TH INTERNATIONAL SYMPOSIUM ON MOBILE MAPPING TECHNOLOGY (Padova) - Conference",
-    45.408661024379434,
-    11.881896761153302
-  ],
-  [
-    "San Nicola Church (Castellaro Lagusello) - survey 2023",
-    45.37299925215895,
-    10.635733806947735
-  ],
-  [
-    "Sabbioneta historic disctrict - survey 2020/2023",
-    44.998129410443035,
-    10.489554071558192
-  ],
-  [
-    "3D-ARCH 2024 (Siena) - Conference",
-    43.315274322812286,
-    11.338807597342935
-  ],
-  [
-    "Villa Mirabellino (Monza) - survey 2024",
-    45.60474869468971,
-    9.276176244129198
-  ] 
+  ["St. Mark's Basilica (Venice) - survey 2018/2025", 45.434594535890966, 12.339742056404038],
+  ["Ducale Palace (Venice) - survey 2025", 45.43372299423483, 12.340385336681752],
+  ["Terracorpo Castle (Caserta) - survey 2023", 41.31368532635118, 14.030226252700746],
+  ["St. Andrea Basilica (Mantova) - monitoring 2018/2025", 45.15890448277335, 10.794386115742203],
+  ["St. Andrea Basilica bell tower (Mantova) - survey", 45.159214, 10.794378],
+  ["St. Peter Cathedral (Mantova) - survey+HBIM 2018", 45.16061990202021, 10.79731519431784],
+  ["Arc of Augusto (Aosta) - survey+HBIM 2019", 45.739321019117064, 7.328146064419771],
+  ["Holy Sepulchre Church (Jerusalem) - survey+HBIM 2021", 31.77850737603422, 35.229602399546174],
+  ["Sabbioneta historic district - survey 2020/2023", 44.998129410443035, 10.489554071558192],
+  ["Palazzo Ducale (Sabbioneta) - exhibition/survey", 44.997493, 10.489152],
+  ["Cortile della Cavallerizza (Mantova) - survey 2018", 45.15992807780407, 10.799964196254171],
+  ["Camera degli Sposi (Mantova) - survey 2020", 45.161124658603605, 10.800016182953703],
+  ["Tempio di San Sebastiano (Mantova) - survey 2019", 45.15105091074389, 10.788113953848386],
+  ["Palazzo dell'Arengario (Monza) - survey 2021", 45.584510534261476, 9.275199938165667],
+  ["Villa Mirabellino (Monza) - survey 2024", 45.60474869468971, 9.276176244129198],
+  ["Roman villa (Negrar di Valpolicella) - survey 2022", 45.53834196254279, 10.945243452280152],
+  ["San Giovanni Evangelista Church (Parma) - survey 2022", 44.80285747339115, 10.332623336224444],
+  ["San Michele Maggiore Church (Pavia) - survey 2023", 45.18221154374779, 9.156673292021898],
+  ["San Nicola Church (Castellaro Lagusello) - survey 2023", 45.37299925215895, 10.635733806947735],
+  ["Old cemetery chapel (San Felice del Benaco) - survey", 45.590900, 10.551500],
+  ["Palazzo ex Licei (Fidenza) - survey", 44.866300, 10.061700],
+  ["Asilo Girotondo (Fidenza) - survey", 44.863500, 10.063500],
+  ["Sanctuary of Beata Vergine della Comuna (Ostiglia) - survey", 45.067300, 11.139900],
+  ["Ponte Musmeci (Potenza) - survey", 40.633600, 15.802800],
+  ["Scuola Media G. Bertazzolo (Mantova) - survey", 45.156900, 10.792700],
+  ["San Maurizio Church (Mantova) - survey", 45.156600, 10.789900],
+  ["Forte Preara (Montorio, Verona) - survey", 45.477800, 11.053900],
+  ["Sanctuary of Beata Vergine Maria delle Grazie (Grazie, Curtatone) - survey", 45.154600, 10.695900],
+  ["Church of the Nativity of the Blessed Virgin Mary (Urago Mella, Brescia) - survey", 45.557600, 10.188600],
+  ["Villa Bocca Trezza (Sommacampagna) - survey", 45.405500, 10.843900],
+  ["Forte Papa (Peschiera del Garda) - survey", 45.436900, 10.693700],
+  ["Forte Sofia (Verona) - thesis case study", 45.453800, 10.988400],
+  ["Villa Forni Cerato (Montecchio Precalcino) - publication case study", 45.663800, 11.568900],
+  ["Padiglione Italia Expo 2015 (Milan) - bachelor thesis", 45.518407, 9.101213],
+  ["University of Brescia (Brescia) - education/seminars", 45.540400, 10.219900],
+  ["IIS G. Bonsignori (Remedello) - education", 45.278700, 10.372500],
+  ["Politecnico di Milano, Mantova Campus - research/teaching/events", 45.156793221796455, 10.781965400912256],
+  ["Politecnico di Milano, Milan - teaching/OpenTalks", 45.478100, 9.227700],
+  ["University of Vigo (Vigo) - research stay 2021", 42.169900, -8.686900],
+  ["Centro Conservazione e Restauro La Venaria Reale (Venaria Reale) - research collaboration", 45.135900, 7.626000],
+  ["3D GeoInfo 2025 (Tokyo) - scientific committee", 35.713700, 139.762200],
+  ["CIPA Symposium 2025 (Seoul) - conference", 37.523900, 126.980300],
+  ["SIFET Conference 2025 (Brindisi) - conference", 40.632700, 17.941800],
+  ["H-BI(M)ON 2025 (Parma) - conference", 44.766296679678184, 10.309571287812796],
+  ["LowCost 3D Workshop 2024 (Brescia) - conference", 45.54408159222205, 10.218860585195127],
+  ["3D GeoInfo 2024 (Vigo) - conference", 42.22740397951454, -8.734829634395538],
+  ["SCANNER Workshop 2024/2025 (Pogoni) - summer school", 40.03377260361343, 20.43838405057237],
+  ["3D-ARCH 2024 (Siena) - conference", 43.315274322812286, 11.338807597342935],
+  ["Digital Heritage 2025 (Siena) - conference", 43.315274322812286, 11.338807597342935],
+  ["GeoAI 2024 (Trento) - conference", 46.06694048848798, 11.15135493340209],
+  ["GeoAI 2023 (Torino) - conference", 45.06756453176935, 7.6563545658953736],
+  ["CIPA Symposium 2023 (Florence) - conference", 43.80098301727668, 11.245345697490501],
+  ["MMT 2023 - 12th International Symposium on Mobile Mapping Technology (Padova) - conference", 45.408661024379434, 11.881896761153302],
+  ["ISPRS Congress 2022 (Nice) - conference", 43.70412963680531, 7.280434813422904],
+  ["3D-ARCH 2022 (Mantova) - conference", 45.156793221796455, 10.781965400912256],
+  ["ARQUEOLOGICA 2.0 & GEORES 2021 (Valencia) - virtual conference", 39.469900, -0.376300],
+  ["CIPA Symposium 2021 (Beijing) - virtual/hybrid conference", 39.904200, 116.407400],
+  ["2GG 2019 (Venice) - conference", 45.434594535890966, 12.339742056404038],
+  ["2GG 2019 (Genova) - conference", 44.407100, 8.933900],
+  ["3D-ARCH 2019 (Bergamo) - conference", 45.698300, 9.677300],
+  ["ISPRS Technical Commission II Symposium 2018 (Riva del Garda) - conference", 45.889200, 10.843100],
+  ["Point Cloud Processing PCP Workshop 2019 (Stuttgart) - conference", 48.74573947433458, 9.108518254847908],
+  ["AI4CH Artificial Intelligence in Geomatics for Cultural Heritage 2019 (Turin) - conference", 45.05353705867574, 7.686619104065213],
+  ["T Hotel (Cagliari) - invited seminar 2024", 39.232200, 9.120500],
+  ["Museo Diocesano Arborense (Oristano) - invited lecture 2025", 39.903000, 8.592100],
+  ["Palazzo Barbieri (Verona) - exhibition 2023/2024", 45.438600, 10.993300],
+  ["San Benedetto Po (Mantova) - Architetture d'Acqua exhibition 2022", 45.039100, 10.928100],
+  ["Startup Weekend Mantova 2025 (Mantova) - event", 45.156793221796455, 10.781965400912256],
+  ["Research and the City (Mantova) - event 2023", 45.160000, 10.797000],
+  ["Spring School Interactive 3D Museums (Bene Vagienna) - training 2019", 44.543200, 7.828400],
+  ["Laboratory of Places Summer School (Ghesc) - training 2017", 46.202900, 8.213200]
 ];
